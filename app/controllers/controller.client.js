ab7985f66396696ad98a340b0620ca5f54f19eac
stepang <g.stepanek.nz@gmail.com>
2015-12-09 04:11:49 +0000

    Remove React because conflict with Masonry, improve Add Photo functionality

--- app/controllers/controller.client.js
+++ app/controllers/controller.client.js
@@ -1,93 +1,6 @@
 'use strict';
 
-function displayPhotos (photos) {
-    $('.grid-holder').empty();
-    ReactDOM.render(
-        <PhotosGrid photos={photos} />,
-        document.getElementById('grid-holder')
-    );
-
-    var $grid = $('.grid').masonry({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        columnWidth: '.grid-sizer',
-        gutter: 10
-    });
-    $grid.imagesLoaded().progress( function() {
-        $grid.masonry();
-    });
-}
-
-var UserImg = React.createClass({
-    handleClick: function(event) {
-        $("li").removeClass("active");
-        $.get(window.location.origin + '/api/photo/user/' + this.props.photo.user.id, function (results) { displayPhotos(results); });  
-    },
-    render: function() {
-        return(
-            <a href="#">
-                <img className="specific-user" 
-                    title={this.props.photo.user.displayName} 
-                    src={this.props.photo.user.photo}
-                    onClick={this.handleClick}>
-                </img>
-            </a>
-        );        
-    }
-});
-
-var RemoveBtn = React.createClass({
-    handleClick: function(event) {
-        var url = window.location.origin + '/api/photo/my?photoid=' + this.props.photo._id
-        $.ajax({url: url, type: 'DELETE', success: function (results) { 
-            $('#my-photos').click();
-        }}); 
-    },
-    render: function() {
-        return(
-            <button className="btn remove-photo" onClick={this.handleClick}>
-                <i className="fa fa-times"></i> Remove
-            </button>            
-        );
-    }
-})
-
-var PhotosGrid = React.createClass({
-  render: function() {
-    var user_link = function(photo) {
-        if( $('#my-photos').hasClass("active") ) {
-            return( <RemoveBtn photo={photo} /> );
-        } else { 
-            return( <UserImg photo={photo} /> );
-        }
-    }
-    
-    var grid_items = this.props.photos.map(function(photo) {
-      return (
-        <div className="grid-item">
-            <a target = "_blank" href={photo.url}>
-                <img className="small-photo" src={photo.url}></img>
-            </a>
-            {user_link(photo)}
-        </div>
-      );
-    });
-    
-    return (
-      <div className="grid">
-        <div className="grid-sizer"></div>
-        {grid_items}
-      </div>
-    );
-  }
-});
-
 (function () {
-    $('#photo-url').keypress(function(e){
-        if(e.keyCode == 13) { 
-            $('#save-photo').click(); 
-        }
-    });
     
     $("document").ready(function () {
         $.get(window.location.origin + '/api/:id', function (user) {
@@ -116,6 +29,45 @@ var PhotosGrid = React.createClass({
         $(menu).addClass("active");
     }
     
+    function displayPhotos (photos) {
+        $('.grid-holder').empty();
+        $('.grid-holder').append('<div class="grid"><div class="grid-sizer"></div></div>');
+        for(var i = photos.length - 1; i >= 0; i--) {
+            var gridItem = '<div class="grid-item"><a href="' + photos[i].url + 
+                '" target = "_blank"><img class="small-photo" src="' + photos[i].url + '"></img></a>';
+            
+            if( $('#my-photos').hasClass("active") ) {
+                gridItem += '<button class="btn remove-photo" id="' + photos[i]._id + '"><i class="fa fa-times"></i> Remove</button>';
+            } else { 
+                gridItem += '<a href="#"><img class="specific-user" id="' + photos[i].user.id + '" title="' + 
+                    photos[i].user.displayName + '" src="' + photos[i].user.photo + '"></img></a>';
+            }
+            $('.grid').append(gridItem + '</div>');
+        }
+
+        $('.specific-user').click( function () {
+            $("li").removeClass("active");
+            $.get(window.location.origin + '/api/photo/user/' + this.id, function (results) { displayPhotos(results); });            
+        });
+        
+        $('.remove-photo').click( function () {
+            var url = window.location.origin + '/api/photo/my?photoid=' + this.id;
+            $.ajax({url: url, type: 'DELETE', success: function (results) { 
+                $('#my-photos').click();
+            }});            
+        });
+        
+        var $grid = $('.grid').masonry({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            columnWidth: '.grid-sizer',
+            gutter: 10
+        });
+        $grid.imagesLoaded().progress( function() {
+            $grid.masonry();
+        });         
+    }
+
     $('#recent-photos').click( function () {
         initialiseMode(this);
         $.get(window.location.origin + '/api/photo/all', function (results) { displayPhotos(results); });
@@ -125,22 +77,37 @@ var PhotosGrid = React.createClass({
         initialiseMode(this);
         $.get(window.location.origin + '/api/photo/my', function (results) { displayPhotos(results); });
     });
-   
+       
+    var placeholder_image = "https://www.mikkis.co.uk/themes/responsive/images/placeholder-500.png";
     $('#add-photo').click( function () {
         $('#photo-url').val("");
-        $('#photo-placeholder').attr("src", "https://www.mikkis.co.uk/themes/responsive/images/placeholder-500.png");
+        $('#photo-placeholder').attr("src", placeholder_image);
         $('#photo-modal').modal();
         setTimeout(function() { $('#photo-url').focus(); }, 500);
     });
     
-    var showPhoto = function () {
-        if($('#photo-url').val().length > 0) {
-            $('#photo-placeholder').attr("src", $('#photo-url').val());
+    $('#photo-placeholder').error(function() {
+        $('#save-photo').prop('disabled', true);
+        $('#photo-placeholder').attr("src", placeholder_image);
+    }).on('load', function() {
+        if($('#photo-placeholder').attr("src") != placeholder_image) {
+            $('#save-photo').prop('disabled', false);            
         }
+    });
+    
+    var showPhoto = function () {
+        $('#photo-placeholder').attr("src", $('#photo-url').val());
     };
     $('#photo-url').change(showPhoto);
+    $('#photo-url').keyup(showPhoto);
     $('#photo-url').bind('paste', function() { setTimeout(function() { showPhoto(); }, 100); });
     
+    $('#photo-url').keypress(function(e){
+        if(e.keyCode == 13 && !$('#save-photo').prop('disabled')) { 
+            $('#save-photo').click(); 
+        }
+    });
+
     $('#save-photo').click( function () {
         var url = window.location.origin + '/api/photo/my?url=' + encodeURIComponent($('#photo-url').val().replace(/["'><]/g, ''));
         $.post(url, function (result) {
